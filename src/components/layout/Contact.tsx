74f0838d30ea1ac14fdeae40cefd03fe7d30f8a4
hellokenta1024 <hellokenta1024@gmail.com>
2025-10-09 19:35:01 -0700

    Fix contact

--- src/components/layout/Contact.tsx
+++ src/components/layout/Contact.tsx
@@ -19,7 +19,7 @@ export default function Contact() {
     const formData = new FormData(e.currentTarget);
 
     try {
-      const response = await fetch("/", {
+      const response = await fetch("/contact-form.html", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: new URLSearchParams(formData as any).toString(),
